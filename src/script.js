776603a160b6ec85702945c1577396cde3f85ab8
MAGV09 <mohamedadel2002320@gmail.com>
2025-12-30 07:32:06 +0200

    refactor: remove unnecessary files and clear html

--- src/script.js
+++ src/script.js
@@ -1,6 +1,3 @@
 import 'modern-normalize/modern-normalize.css';
 import './styles/reset.css';
 import './styles/style.css';
-import {oi} from './test.js'
-
-console.log('oi');
--- src/test.js
+++
@@ -1,3 +0,0 @@
-const oi = 'oi'
-
-export {oi}
