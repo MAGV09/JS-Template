4566937c314cd1d7b3720fc72ee9a17f61ac9f1b
MAGV09 <mohamedadel2002320@gmail.com>
2026-01-08 15:08:17 +0200

    fix: remove unnecessary files

--- src/script.js
+++ src/script.js
@@ -1,10 +1,3 @@
 import 'modern-normalize/modern-normalize.css';
 import './styles/reset.css';
 import './styles/style.css';
-import {superHeroes} from './test.js'
-
-const obj =JSON.parse('{"name":"John", "age":30, "city":"New York"}')
-console.log(superHeroes.homeTown);
-console.log(obj.name);
-const str = JSON.stringify(obj)
-console.log(str);
--- test/script.js
+++
@@ -1,6 +0,0 @@
-fetch('heroes.json')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data.squadName); // "Super hero squad"
-    console.log(data.members[0].name); // "Molecule Man"
-  });
